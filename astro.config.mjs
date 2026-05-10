f3df081bbcb818fe4ca52b10ce7b7436fa9fb91a
alvarosaid10 <alvaroduartesandoval@gmail.com>
2026-05-10 21:15:04 +0000

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://cloverpropiedades.cl",
+  base: "./",
   output: "static",
   integrations: [
     tailwind({
