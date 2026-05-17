ea18ee0b7ef02716c0794f7154d432c354b9a9f7
Alvaro Duarte <aduarte@acantoteknos.com>
2026-05-17 14:11:24 -0400

    Redisenar landing corporativa Clover

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://cloverpropiedades.cl",
-  base: "./",
   output: "static",
   build: {
     format: "directory"
